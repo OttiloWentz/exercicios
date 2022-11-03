23729ee55dc3a20873693ede381c11273d7eafed
OttiloWentz <o.wentz@hotmail.com>
2022-11-03 19:03:12 -0300

    conclusão dos exercícios swagger

--- pedidos-springboot-dto/src/main/java/br/com/exercicios/dto/repository/ProdutoRepository.java
+++ pedidos-springboot-dto/src/main/java/br/com/exercicios/dto/repository/ProdutoRepository.java
@@ -3,9 +3,11 @@ package br.com.exercicios.dto.repository;
 import br.com.exercicios.dto.model.Produto;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProdutoRepository extends CrudRepository<Produto, Long> {
 
     @Query("select p from Produto p where p.descricao like %?1%")
